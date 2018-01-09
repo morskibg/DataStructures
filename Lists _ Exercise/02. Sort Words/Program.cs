2a7db123edf64fcda2cb13b085236c8b129337e7
morskibg <dimityrp@yahoo.com>
2018-01-09 20:33:05 +0200

    +

---
+++ Lists _ Exercise/02. Sort Words/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Sort_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            if (line != String.Empty)
+            {
+                List<string> words = line.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => x.Trim())
+                    .OrderBy(x => x)
+                    .ToList();
+                Console.WriteLine(string.Join(" ", words));
+
+            }
+        }
+    }
+}
