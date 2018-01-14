1dd6806cee2260394da473a2eb25a4d8dbcdd10d
morskibg <dimityrp@yahoo.com>
2018-01-14 20:27:16 +0200

    +

---
+++ StackQueuesExercise/Problem4. LinkedStack_v2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace Problem4.LinkedStack_v2
+//{
+    public class LinkedStack<T>
+    {
+        private Node<T> firstNode;
+
+        public LinkedStack()
+        {
+            this.firstNode = new Node<T>();
+            this.Count = 0;
+        }
+        public int Count { get; private set; }
+
+        public void Push(T element)
+        {
+            Node<T> newNode = new Node<T>(element);
+            newNode.NextNode = firstNode.NextNode;
+            this.firstNode.NextNode = newNode;
+            ++this.Count;
+        }
+
+        public T Pop()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            T item = this.firstNode.NextNode.value;
+            --this.Count;
+            firstNode = firstNode.NextNode;
+            return item;
+        }
+
+        public T[] ToArray()
+        {
+            T[] arr = new T[this.Count];
+            int idx = 0;
+            while (this.Count > 0)
+            {
+                arr[idx] = this.Pop();
+                ++idx;
+            }
+            return arr;
+        }
+        private class Node<T>
+        {
+            public T value;
+            public Node<T> NextNode { get; set; }
+
+            public Node(T value, Node<T> nextNode = null)
+            {
+                this.value = value;
+                this.NextNode = null;
+            }
+
+            public Node()
+            {
+                this.NextNode = null;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedStack<int> stack = new LinkedStack<int>();
+            stack.Push(100);
+            stack.Push(99);
+            stack.Push(98);
+            //Console.WriteLine(stack.Pop());
+            //Console.WriteLine(stack.Pop());
+            //Console.WriteLine(stack.Pop());
+            //Console.WriteLine(stack.Count);
+
+            Console.WriteLine(string.Join(" ", stack.ToArray()));
+            //Console.WriteLine();
+        }
+    }
+//}
---
+++ StackQueuesExercise/Problem8.N-M/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem8.N_M
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var userInput = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var n = userInput[0];
+            var m = userInput[1];
+
+            var queue = new Queue<MyItem<int>>();
+            queue.Enqueue(new MyItem<int>(n));
+
+            var result = new Stack<int>();
+            while (queue.Count > 0)
+            {
+                var item = queue.Dequeue();
+                if (item.Value < m)
+                {
+                    queue.Enqueue(new MyItem<int>(item.Value + 1, item));
+                    queue.Enqueue(new MyItem<int>(item.Value + 2, item));
+                    queue.Enqueue(new MyItem<int>(item.Value * 2, item));
+                }
+
+                if (item.Value == m)
+                {
+                    var currentItem = item;
+                    while (currentItem != null)
+                    {
+                        result.Push(currentItem.Value);
+                        currentItem = currentItem.Prev;
+                    }
+
+                    break;
+                }
+            }
+
+            if (result.Count > 0)
+            {
+                Console.WriteLine(string.Join(" -> ", result));
+            }
+            else
+            {
+                //Console.WriteLine("(no solution)");
+            }
+        }
+
+        private class MyItem<T>
+        {
+            public MyItem(
+                T value,
+                MyItem<T> prev = null)
+            {
+                this.Value = value;
+                this.Prev = prev;
+            }
+
+            public T Value { get; private set; }
+
+            public MyItem<T> Prev { get; private set; }
+        }
+    }
+    
+}
