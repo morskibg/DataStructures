37e057b7305abb7d69a16ff808c3a1b329eabfb9
morskibg <dimityrp@yahoo.com>
2018-01-16 21:16:34 +0200

    +

---
+++ Lists _ Exercise/Problem 6. Implement the Data Structure ReversedList/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace Problem_6.Implement_the_Data_Structure_ReversedList
+//{
+    public class ReversedList<T> : IEnumerable<T>
+    {
+        private T[] arr { get; set; }
+        private const int InitialSize = 2;
+        public int Count { get; private set; }
+
+        public ReversedList()
+        {
+            this.arr = new T[InitialSize];
+            this.Count = 0;
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return this.arr.Length;
+            }
+            set { }
+        }
+
+        public void Add(T item)
+        {
+            if (this.Capacity == this.Count)
+            {
+                T[] newArr = new T[this.Capacity * 2];
+                for (int i = 0; i < this.Count; i++)
+                {
+                    newArr[i] = this.arr[i];
+                }
+                this.arr = newArr;
+                this.Capacity *= 2;
+            }
+            this.arr[this.Count] = item;
+            ++this.Count;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index >= 0 && index < this.Count)
+            {
+                int modIdx = this.Count - 1 - index;
+                for (int i = modIdx; i < this.Count - 1; i++)
+                {
+                    this.arr[i] = this.arr[i + 1];
+                }
+                --this.Count;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+        public T this[int index]
+        { 
+            get
+            {
+                if (index >= 0 && index < this.Count)
+                {
+                    return this.arr[(this.Count - 1) - index];
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+            set
+            {
+                if (index >= 0 && index < this.Count)
+                {
+                    this.arr[index] = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+            }
+
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                yield return this.arr[this.Count - 1 - i];
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ReversedList<int> revList = new ReversedList<int>();
+            //Console.WriteLine(revList.Count);
+            //revList.Add(1);
+            //revList.Add(2);
+            //revList.Add(3);
+            //revList.Add(4);
+            //revList.Add(5);
+            //Console.WriteLine(revList[0]);
+            //revList.RemoveAt(0);
+            //foreach (var item in revList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //int t = 0;
+        }
+    }
+//}
---
+++ Lists _ Exercise/Problem 7. Distance in Labyrinth/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_7.Distance_in_Labyrinth
+{
+    class Program
+    {
+        static bool IsInBounds(int row, int col, int size)
+        {
+            if (row >= 0 && row < size && col >= 0 && col < size)
+            {
+                return true;
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char [,] matrix = new char[n,n];
+            int startRow = -1;
+            int startCol = -1;
+
+            for (int i = 0; i < n; ++i)
+            {
+                char[] lineOfChars = Console.ReadLine().ToCharArray();
+
+                for (int j = 0; j < n; ++j)
+                {
+
+                    if (lineOfChars[j] == '*')
+                    {
+                        startCol = j;
+                        startRow = i;
+                    }
+                    else if (lineOfChars[j] == '0')
+                    {
+                        matrix[i, j] = 'u';
+                    }
+                    else
+                    {
+                        matrix[i, j] = 'x';
+                    }
+                }
+            }
+            int steps = 0;
+
+        }
+    }
+}
